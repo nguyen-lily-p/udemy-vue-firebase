827ce907d4c6229939689c9c869fff84b22534b0
Lily Nguyen <nguyen.lily.p@gmail.com>
2018-07-05 14:43:37 -0700

    changed to be template files

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'Becoming a Vue Ninja',
-        name: 'Ryu',
-        url: 'http://www.youtube.com',
-        classes: ['one', 'two']
-    },
-    methods: {
-        greet(time){
-            return `Hello and good ${time}, ${this.name}`
-        }
+        title: 'Becoming a Vue Ninja'
     }
 })
